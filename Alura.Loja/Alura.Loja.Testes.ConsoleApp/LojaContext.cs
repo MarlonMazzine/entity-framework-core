28f1a2515ce4a8eedb026a28f967e99c95cd3b9d
marlon.dsn.pack <marlon.dsn.pack@alterdata.com.br>
2021-07-21 20:10:28 -0300

    finalização do curso de Entity Framework - part 1

--- Alura.Loja/Alura.Loja.Testes.ConsoleApp/LojaContext.cs
+++ Alura.Loja/Alura.Loja.Testes.ConsoleApp/LojaContext.cs
@@ -6,6 +6,29 @@ namespace Alura.Loja.Testes.ConsoleApp
     public class LojaContext : DbContext
     {
         public DbSet<Produto> Produtos { get; set; }
+        public DbSet<Compra> Compras { get; set; }
+        public DbSet<Promocao> Promocoes { get; set; }
+        public DbSet<Cliente> Clientes { get; internal set; }
+
+        //Dessa maneira eu faço chave primária composta
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder
+                .Entity<PromocaoProduto>()
+                .HasKey(pp => new { pp.PromocaoId, pp.ProdutoId });
+
+            modelBuilder
+                .Entity<Endereco>()
+                .ToTable("Enderecos");
+
+            modelBuilder
+                .Entity<Endereco>()
+                .Property<int>("ClienteId");
+
+            modelBuilder
+                .Entity<Endereco>()
+                .HasKey("ClienteId");
+        }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
--- Alura.Loja/Alura.Loja.Testes.ConsoleApp/Program.cs
+++ Alura.Loja/Alura.Loja.Testes.ConsoleApp/Program.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore.ChangeTracking;
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,32 +12,116 @@ namespace Alura.Loja.Testes.ConsoleApp
         {
             using (var contexto = new LojaContext())
             {
-                var produtos = contexto.Produtos.ToList();
+                var cliente = contexto
+                    .Clientes
+                    .Include(c => c.EnderecoDeEntrega)
+                    .FirstOrDefault();
 
-                EixibirEntries(contexto.ChangeTracker.Entries());
+                Console.WriteLine($"Endereço de entrega: {cliente.EnderecoDeEntrega.Logradouro}");
 
-                var novoProduto = new Produto()
+                var produto = contexto
+                   .Produtos
+                   .Where(p => p.Id == 3002)
+                   .FirstOrDefault();
+
+                contexto.Entry(produto)
+                    .Collection(p => p.Compras)
+                    .Query()
+                    .Where(c => c.Preco > 10)
+                    .Load();
+
+                Console.WriteLine($"Mostrando as compras do produto {produto.Nome}");
+                foreach (var item in produto.Compras)
                 {
-                    Nome = "Sabão em pó",
-                    Categoria = "Limpeza",
-                    Preco = 4.99
-                };
+                    Console.WriteLine(item);
+                }
 
-                contexto.Produtos.Add(novoProduto);
-                contexto.Remove(novoProduto);
-                EixibirEntries(contexto.ChangeTracker.Entries());
-                contexto.SaveChanges();
-                EixibirEntries(contexto.ChangeTracker.Entries());
+
             }
         }
 
-        private static void EixibirEntries(IEnumerable<EntityEntry> entries)
+        private static void ExibirProdutosDaPromocao()
         {
-            Console.WriteLine("=====================");
-
-            foreach (var e in entries)
+            using (var contexto2 = new LojaContext())
             {
-                Console.WriteLine(e.Entity.ToString() + " - " + e.State);
+                var promocao = contexto2
+                    .Promocoes
+                    .Include(p => p.Produtos)
+                    .ThenInclude(pp => pp.Produto)
+                    .FirstOrDefault();
+                Console.WriteLine("\nMotrando os produtos da promoção...");
+
+                foreach (var item in promocao.Produtos)
+                {
+                    Console.WriteLine(item.Produto);
+                }
+            }
+        }
+
+        private static void IncluirPromocao()
+        {
+            using (var contexto = new LojaContext())
+            {
+                var promocao = new Promocao();
+                promocao.Descricao = "Queima Total 2017";
+                promocao.DataInicio = new DateTime(2017, 1, 1);
+                promocao.DataTermino = new DateTime(2017, 1, 31);
+
+                var produtos = contexto
+                    .Produtos
+                    .Where(p => p.Categoria == "Bebidas")
+                    .ToList();
+
+                foreach (var item in produtos)
+                {
+                    promocao.IncluiProduto(item);
+                }
+
+                contexto.Promocoes.Add(promocao);
+                contexto.SaveChanges();
+            }
+        }
+
+        private static void UmPraUm()
+        {
+            var fulano = new Cliente();
+            fulano.Nome = "Fulaninho de Tal";
+
+            fulano.EnderecoDeEntrega = new Endereco()
+            {
+                Numero = 12,
+                Logradouro = "Rua dos Inválidos",
+                Complemento = "sobrado",
+                Bairro = "Centro",
+                Cidade = "Cidade"
+            };
+
+            using (var contexto = new LojaContext())
+            {
+                contexto.Clientes.Add(fulano);
+                contexto.SaveChanges();
+            }
+        }
+
+        private static void MuitosParaMuitos()
+        {
+            var p1 = new Produto() { Nome = "Suco de Laranja", Categoria = "Bebidas", PrecoUnitario = 8.79, Unidade = "Litros" };
+            var p2 = new Produto() { Nome = "Café", Categoria = "Bebidas", PrecoUnitario = 12.45, Unidade = "Gramas" };
+            var p3 = new Produto() { Nome = "Macarrão", Categoria = "Alimentos", PrecoUnitario = 4.23, Unidade = "Gramas" };
+
+            Promocao promocaoDePascoa = new Promocao();
+            promocaoDePascoa.Descricao = "Pácoa Feliz";
+            promocaoDePascoa.DataInicio = DateTime.Now;
+            promocaoDePascoa.DataTermino = DateTime.Now.AddMonths(3);
+            promocaoDePascoa.IncluiProduto(p1);
+            promocaoDePascoa.IncluiProduto(p2);
+            promocaoDePascoa.IncluiProduto(p3);
+
+            using (var contexto = new LojaContext())
+            {
+                var promocao = contexto.Promocoes.Find(2);
+                contexto.Promocoes.Remove(promocao);
+                contexto.SaveChanges();
             }
         }
     }
